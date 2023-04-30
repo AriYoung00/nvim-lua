dc025f37dfb79d02dc35a96fa3ce62e3502365a7
Ariel Young <ariyoung@pm.me>
2023-04-29 21:29:16 -0700

    Removed autopairs '<>' rule for Rust

--- lua/plugins/setup.lua
+++ lua/plugins/setup.lua
@@ -147,9 +147,8 @@ return require('packer').startup(function(use)
         config = function() 
             local npairs = require('nvim-autopairs')
             local Rule = require('nvim-autopairs.rule')
-            npairs.setup { check_ts = true }
             npairs.add_rules(require('nvim-autopairs.rules.endwise-lua'))
-            npairs.add_rules({ Rule('<', '>', {'rs', 'rust'}) })
+            npairs.setup { check_ts = true }
         end
     }
 
