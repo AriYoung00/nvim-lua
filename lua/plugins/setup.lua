bfa4435e52da3b2dfe81bd8ee8f4b38c1d4f9322
Ariel Young <ariel.young@modalai.com>
2023-07-11 11:13:49 -0700

    remove python from treesitter ensure_installed

--- lua/plugins/setup.lua
+++ lua/plugins/setup.lua
@@ -184,7 +184,7 @@ return require('packer').startup(function(use)
             }
 
             require('nvim-treesitter.configs').setup {
-                ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "rust", "python" },
+                ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "rust"},
 
                 rainbow = {
                     enable = true,
